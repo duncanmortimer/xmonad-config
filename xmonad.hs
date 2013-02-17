3c5cf5ca244944f63570db740c4ed5102d34cb8a
Duncan Mortimer <dmorti@gmail.com>
2013-02-17 11:30:07 +0000

    Added a bunch of stuff I haven't recorded
    - Key change: EWMH activated --- for using arbtt time tracker

--- xmonad.hs
+++ xmonad.hs
@@ -1,8 +1,37 @@
+{- Copied shamelessly from: http://www.haskell.org/haskellwiki/Xmonad/Config_archive/John_Goerzen's_Configuration#Installing_xmobar
+-}
+
 import XMonad
-import XMonad.Util.EZConfig
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
 
-main = xmonad $ defaultConfig { terminal = "lxterminal" } 
-		`additionalKeysP` myKeys
+import XMonad.Hooks.EwmhDesktops -- to get Chrome to work
 
-myKeys = [ 
+-- allow use of e.g. volume control keys
+import Graphics.X11.ExtraTypes.XF86
+
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeysP)
+
+import System.IO
+
+main :: IO ()
+main = do xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmobarrc"
+          xmonad $ ewmh defaultConfig 
+            { manageHook = manageDocks <+> manageHook defaultConfig
+            , layoutHook = avoidStruts $ layoutHook defaultConfig
+            , handleEventHook = fullscreenEventHook
+            , logHook = dynamicLogWithPP xmobarPP
+              { ppOutput = hPutStrLn xmproc
+              , ppTitle = xmobarColor "green" "" . shorten 50
+              }
+            , modMask = mod3Mask  -- use capslock as mod key
+            , terminal = "lxterminal" } 
+            `additionalKeysP` myKeys
+
+myKeys = [ ( "<XF86AudioLowerVolume>", spawn "amixer -q sset Master 5%- unmute")
+         , ( "<XF86AudioRaiseVolume>", spawn "amixer -q sset Master 5%+ unmute")
+         , ( "<XF86AudioMute>", spawn "amixer -q sset Master toggle")
+--       , ( "<XF86XK_MonBrightnessUp>", spawn "xbacklight +20")
+--       , ( "<XF86XK_MonBrightnessDown>", spawn "xbacklight -20")
 	 ]
